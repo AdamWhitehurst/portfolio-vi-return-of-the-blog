59b1e396cf01f613ee141099dbb194500b681e84
Adam Whitehurst <Adam.Whitehurst@live.com>
2020-12-03 09:43:47 -0800

    FEAT: 📝 Markdown styling and highlighting

--- src/hook/useMd.js
+++ src/hook/useMd.js
@@ -1,10 +1,23 @@
+import hljs from 'highlight.js'
 import MarkdownIt from 'markdown-it'
 import wikilinks from 'markdown-it-wikilinks'
+import { ToastsStore } from 'react-toasts'
 
 export function useMd() {
   return new MarkdownIt({
     linkify: true,
     html: true,
     breaks: true,
+    highlight(str, lang) {
+      if (lang && hljs.getLanguage(lang)) {
+        try {
+          return hljs.highlight(lang, str).value
+        } catch (e) {
+          ToastsStore.error(e)
+        }
+      }
+
+      return '' // use external default escaping
+    },
   }).use(wikilinks)
 }
